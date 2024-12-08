b18a97555eb9b53ea8db80e8290760ec80a25bb5
Mohamed Arafath <111099723+DevArafath@users.noreply.github.com>
2024-12-08 07:55:16 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,11 +37,35 @@ $(document).ready(function () {
     // WhatsApp function to open chat ######################################
     function openWhatsApp() {
         const phoneNumber = "+97477375047"; // Replace with the phone number in international format
-        const message = encodeURIComponent("Hi! I would like to know more about Al-Shaheen Medical Center"); // Optional preset message
+        const message = encodeURIComponent("Hi! I would like to know more about you. and lets discuss the project"); // Optional preset message
         const url = `https://wa.me/${phoneNumber}?text=${message}`;
         window.open(url, "_blank");
     }
     // Attach the WhatsApp function to the button click event
     whatsappButton.onclick = openWhatsApp;
+
+
+    // Change background image based on time of day #######################
+    const heroSection = document.querySelector(".hero");
+    const morningImage = "./assets/img/morning.jpg";
+    const afternoonImage = ".assets/img/afternoon.jpg";
+    const eveningImage = ".assets/img/evening.jpg";
+    const nightImage = ".assets/img/night.jpg";
+
+    const currentHour = new Date().getHours();
+
+    if (currentHour >= 6 && currentHour < 12) {
+        // Morning (6 AM - 12 PM)
+        heroSection.style.backgroundImage = `url('${morningImage}')`;
+    } else if (currentHour >= 12 && currentHour < 18) {
+        // Afternoon (12 PM - 6 PM)
+        heroSection.style.backgroundImage = `url('${afternoonImage}')`;
+    } else if (currentHour >= 18 && currentHour < 21) {
+        // Evening (6 PM - 9 PM)
+        heroSection.style.backgroundImage = `url('${eveningImage}')`;
+    } else {
+        // Night (9 PM - 6 AM)
+        heroSection.style.backgroundImage = `url('${nightImage}')`;
+    }
     
 });
